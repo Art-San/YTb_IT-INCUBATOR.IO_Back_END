91c6f7134e17fe20b7585a14c7b3c67ea471fe9d
Aleksander <asava@sibmail.com>
2023-06-26 14:06:37 +0700

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,38 @@
+import bodyParser from 'body-parser'
 import express, { Request, Response } from 'express'
+import cors from 'cors'
+
+const corsMiddlewars = cors()
+app.use(corsMiddlewars)
+const jsonBodyMiddleware = bodyParser.json()
+app.use(jsonBodyMiddleware)
+
+// const port = process.env.PORT || 5000
+
+let videos = [
+  { id: 1, title: 'About JS - 01', author: 'it-incubatur.ru' },
+  { id: 2, title: 'About JS - 02', author: 'it-incubatur.ru' },
+  { id: 3, title: 'About JS - 03', author: 'it-incubatur.ru' },
+  { id: 4, title: 'About JS - 04', author: 'it-incubatur.ru' },
+  { id: 5, title: 'About JS - 05', author: 'it-incubatur.ru' },
+  { id: 6, title: 'About JS - 06', author: 'it-incubatur.ru' }
+]
 
 const app = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-  let helloMessege = 'Hello Incubatur'
-  res.send(helloMessege)
+  debugger
+  res.send('Hello IT-INCUBATOR')
 })
 
+// app.get('/videos', (req: Request, res: Response) => {}
+// app.post('/videos', (req: Request, res: Response) => {}
+// app.put('/videos/:videoId', (req: Request, res: Response) => {}
+// app.get('/videos/:videoId', (req: Request, res: Response) => {}
+// app.delete('/videos/:videoId', (req: Request, res: Response) => {}
+
+// старт app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
