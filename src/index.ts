05037b155678b31b9aebb5ba0bb93f31cf0210e1
Aleksander <asava@sibmail.com>
2023-06-26 16:44:13 +0700

    ok

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import bodyParser from 'body-parser'
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 
+// создали express app
+const app = express()
+
 const corsMiddlewars = cors()
 app.use(corsMiddlewars)
 const jsonBodyMiddleware = bodyParser.json()
 app.use(jsonBodyMiddleware)
 
-// const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 let videos = [
   { id: 1, title: 'About JS - 01', author: 'it-incubatur.ru' },
@@ -15,18 +18,20 @@ let videos = [
   { id: 3, title: 'About JS - 03', author: 'it-incubatur.ru' },
   { id: 4, title: 'About JS - 04', author: 'it-incubatur.ru' },
   { id: 5, title: 'About JS - 05', author: 'it-incubatur.ru' },
+  { id: 6, title: 'About JS - 06', author: 'it-incubatur.ru' },
   { id: 6, title: 'About JS - 06', author: 'it-incubatur.ru' }
 ]
 
-const app = express()
-const port = 3000
+// const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-  debugger
-  res.send('Hello IT-INCUBATOR')
+  res.send('Hello IT-INCUBATOR!!!!')
 })
 
-// app.get('/videos', (req: Request, res: Response) => {}
+app.get('/videos', (req: Request, res: Response) => {
+  const arrName = videos.map((video) => video.title)
+  res.send(arrName)
+})
 // app.post('/videos', (req: Request, res: Response) => {}
 // app.put('/videos/:videoId', (req: Request, res: Response) => {}
 // app.get('/videos/:videoId', (req: Request, res: Response) => {}
