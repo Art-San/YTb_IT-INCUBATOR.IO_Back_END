428e5f216b8b9303c0f51c35f0cd6c6129c8bbf5
Aleksander <asava@sibmail.com>
2023-06-28 13:25:08 +0700

    ok

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,6 @@ const addresses = [
   { id: 2, value: 'Selickaga 11' }
 ]
 
-/*получение всех продуктов первый вар */
-// app.get('/products', (req: Request, res: Response) => {
-//   res.send(products)
-// })
-
 /*(?title=man query параметр) фильтрыция http://localhost:3001/products?title=man  */
 app.get('/products', (req: Request, res: Response) => {
   if (req.query.title) {
@@ -30,6 +25,18 @@ app.get('/products', (req: Request, res: Response) => {
   } else {
     res.send(products)
   }
+})
+
+/*Создание добавление продукта */
+app.post('/products', (req: Request, res: Response) => {
+  const newProdyct = {
+    id: +new Date(),
+    title: req.body.title,
+    price: req.body.price
+  }
+  products.push(newProdyct)
+
+  res.status(201).send(products)
 })
 
 /**получение продукта по :id url params */
@@ -43,28 +50,6 @@ app.get('/products/:id', (req: Request, res: Response) => {
   }
 })
 
-/**удаление по req.params с помощью цикла FOR */
-app.delete('/products/:id', (req: Request, res: Response) => {
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].id === +req.params.id) {
-      products.splice(i, 1)
-      res.send(204)
-      return
-    }
-  }
-  res.send(404)
-})
-/*Создание добавление продукта */
-app.post('/products', (req: Request, res: Response) => {
-  const newProdyct = {
-    id: +new Date(),
-    title: req.body.title,
-    price: req.body.price
-  }
-  products.push(newProdyct)
-
-  res.status(201).send(products)
-})
 /*Обновление продукта продукта */
 app.put('/products/:id', (req: Request, res: Response) => {
   const product = products.find((p) => p.id === +req.params.id)
@@ -79,72 +64,21 @@ app.put('/products/:id', (req: Request, res: Response) => {
   }
 })
 
-/*Обновление продукта продукта НАПИСАЛ ИИ */
-// ====================== вар второй
-// app.put('/products/:id', (req: Request, res: Response) => {
-//   const productId = Number(req.params.id)
-//   const productIndex = products.findIndex((p) => p.id === productId)
-
-//   if (productIndex !== -1) {
-//     products[productIndex] = {
-//       ...products[productIndex],
-//       title: req.body.title,
-//       price: req.body.price
-//     }
-//     res.send(products[productIndex])
-//   } else {
-//     res.status(404).send(`Product with ID ${productId} not found`)
-//   }
-// })
-
-// ====================== вар первый
-// app.put('/products/:id', (req: Request, res: Response) => {
-//   const productId = parseInt(req.params.id)
-//   const productIndex = products.findIndex((p) => p.id === productId)
-
-//   if (productIndex === -1) {
-//     return res.status(404).json({ error: 'Product not found' })
-//   }
-
-//   const { title, price } = req.body
-
-//   if (!title || typeof title !== 'string') {
-//     return res.status(400).json({ error: 'Invalid product title' })
-//   }
-
-//   if (!price || typeof price !== 'string') {
-//     return res.status(400).json({ error: 'Invalid product price' })
-//   }
-
-//   products[productIndex] = {
-//     ...products[productIndex],
-//     title,
-//     price
-//   }
-
-//   // здесь вы можете использовать методы для сохранения изменений в базе данных или файловой системе
-
-//   res.send(products[productIndex])
-// })
-
-/**удаление по req.params с помощью FILTER */
-// app.delete('/products/:id', (req: Request, res: Response) => {
-//   const data = req.params.id
-//   let product = products.filter((p) => p.id !== +data)
-//   res.send(product)
-// })
-
-/* получение продукта req.params и FIND */
-app.get('/products/:productTitle', (req: Request, res: Response) => {
-  const data = req.params.productTitle
-  let product = products.find((g) => g.title === data)
-  if (product) {
-    res.send(product)
-  } else {
-    res.send(404)
+/**удаление по req.params с помощью цикла FOR */
+app.delete('/products/:id', (req: Request, res: Response) => {
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === +req.params.id) {
+      products.splice(i, 1)
+      res.send(204)
+      return
+    }
   }
+  res.send(404)
 })
 
+app.get('/addresses', (req: Request, res: Response) => {
+  res.send(addresses)
+})
 app.get('/addresses/:id', (req: Request, res: Response) => {
   const adress = addresses.find((a) => a.id === Number(req.params.id))
   if (adress) {
