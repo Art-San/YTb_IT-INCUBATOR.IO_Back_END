c651f614149ceeba5d7fffefc8a0b75dc866b9dc
Aleksander <asava@sibmail.com>
2023-06-29 14:04:59 +0700

    layered architecture

--- src/repositories/products.repository.ts
+++ src/repositories/products.repository.ts
@@ -6,14 +6,47 @@ const products = [
 ]
 
 export const productsRepository = {
-  findProducts(title: string | null) {
+  findProducts(title: string | null | undefined) {
     if (title) {
       const filtredProduct = products.filter((p) => p.title.indexOf(title) > -1)
       return filtredProduct
     } else {
       return products
     }
+  },
+  createProduct(title: string, price: string) {
+    const newProdyct = {
+      id: +new Date(),
+      title: title,
+      price: price
+    }
+    products.push(newProdyct)
+    return products
+  },
+  getProductById(id: number) {
+    let product = products.find((p) => p.id === id)
+    return product
+  },
+  updateProduct(id: number, title: string, price: string) {
+    const product = products.find((p) => p.id === id)
+    if (product) {
+      product.title = title
+      product.price = price
+      // return product
+      return products
+    } else {
+      return 404
+    }
+  },
+  deleteProduct(id: number) {
+    console.log(typeof id)
+    for (let i = 0; i < products.length; i++) {
+      if (products[i].id === id) {
+        products.splice(i, 1)
+        return `продукт ${products[i].title} удален`
+      }
+    }
+    return 404
   }
 }
-
 // 19:19
--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -7,62 +7,45 @@ export const productsRouter = Router({})
 productsRouter.get('/', (req: Request, res: Response) => {
   console.log(req.query.title)
   const faundProduct = productsRepository.findProducts(
-    req.query.title ? req.query.title.toString() : null
+    req.query.title?.toString()
   )
   res.send(faundProduct)
-  // if (req.query.title) {
-  //   let searchString = req.query.title.toString()
-  //   res.send(products.filter((p) => p.title.indexOf(searchString) > -1))
-  // } else {
-  //   res.send(products)
-  // }
 })
 
 /*Создание добавление продукта */
-// productsRouter.post('/', (req: Request, res: Response) => {
-//   const newProdyct = {
-//     id: +new Date(),
-//     title: req.body.title,
-//     price: req.body.price
-//   }
-//   products.push(newProdyct)
-
-//   res.status(201).send(products)
-// })
+productsRouter.post('/', (req: Request, res: Response) => {
+  const { title, price } = req.body
+  const allProducts = productsRepository.createProduct(title, price)
+  res.status(201).send(allProducts)
+})
 
 /**получение продукта по :id url params */
-// productsRouter.get('/:id', (req: Request, res: Response) => {
-//   const data = req.params.id
-//   let product = products.find((p) => p.id === +data)
-//   if (product) {
-//     res.send(product)
-//   } else {
-//     res.send(404)
-//   }
-// })
+productsRouter.get('/:id', (req: Request, res: Response) => {
+  const product = productsRepository.getProductById(+req.params.id)
+  if (product) {
+    res.send(product)
+  } else {
+    res.send(404)
+  }
+})
 
 /*Обновление продукта продукта */
-// productsRouter.put('/:id', (req: Request, res: Response) => {
-//   const product = products.find((p) => p.id === +req.params.id)
-//   if (product) {
-//     product.title = req.body.title
-//     product.price = req.body.price
-//     // res.send(product)
-//     res.send(products)
-//   } else {
-//     // res.send(404)
-//     res.status(404).send({ error: 'Product not found' })
-//   }
-// })
+productsRouter.put('/:id', (req: Request, res: Response) => {
+  const id = +req.params.id
+  const { title, price } = req.body
+  const allProducts = productsRepository.updateProduct(id, title, price)
+  res.send(allProducts)
+})
 
 /**удаление по req.params с помощью цикла FOR */
-// productsRouter.delete('/:id', (req: Request, res: Response) => {
-//   for (let i = 0; i < products.length; i++) {
-//     if (products[i].id === +req.params.id) {
-//       products.splice(i, 1)
-//       res.send(204)
-//       return
-//     }
-//   }
-//   res.send(404)
-// })
+productsRouter.delete('/:id', (req: Request, res: Response) => {
+  const answer = productsRepository.deleteProduct(+req.params.id)
+  // for (let i = 0; i < products.length; i++) {
+  //   if (products[i].id === +req.params.id) {
+  //     products.splice(i, 1)
+  //     res.send(204)
+  //     return
+  //   }
+  // }
+  res.send(answer)
+})
