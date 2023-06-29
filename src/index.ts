c394318aa1b2ed4c6a56daed1bed5fb354d58bd4
Aleksander <asava@sibmail.com>
2023-06-29 23:17:37 +0700

    middleware

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,56 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { productsRouter } from './routes/products-router'
 import { adressRouter } from './routes/addresses-router'
 
 // создали express app
 const app = express()
-app.use(express.json())
 
 const port = process.env.PORT || 3001
 
-app.use('/products', productsRouter)
-app.use('/addresses', adressRouter)
+app.get(
+  '/products',
+  (req: Request, res: Response, next: NextFunction) => {
+    // @ts-ignore
+    req.blabla = 'hellou'
+    next()
+  },
+  (req: Request, res: Response) => {
+    // @ts-ignore
+    const blabla = req.blabla
+    res.send({ value: blabla })
+  }
+)
 
 // старт app
 app.listen(port, () => {
   console.log(`Example app listening on port: ${port}`)
 })
 
-// С post- create- создать
-// R get - reade - читать
-// U put - update- обновить
-// D del - delete -удалить
+// 14:11
 
-// Presentation Layer
-// Business Layer
-// Data Access Layer
+// import express from 'express'
+// import { productsRouter } from './routes/products-router'
+// import { adressRouter } from './routes/addresses-router'
+
+// // создали express app
+// const app = express()
+// app.use(express.json())
+
+// const port = process.env.PORT || 3001
+
+// app.use('/products', productsRouter)
+// app.use('/addresses', adressRouter)
+
+// // старт app
+// app.listen(port, () => {
+//   console.log(`Example app listening on port: ${port}`)
+// })
+
+// // С post- create- создать
+// // R get - reade - читать
+// // U put - update- обновить
+// // D del - delete -удалить
+
+// // Presentation Layer
+// // Business Layer
+// // Data Access Layer
